46fabc593a9d7ae3e93316d351d31e89cd558df2
Mikael Lindemann <mikael@twig.energy>
2024-09-16 14:11:42 +0200

    One client benchmark to rule them all

--- benchmarks/source/client_benchmarks.cpp
+++ benchmarks/source/client_benchmarks.cpp
@@ -1,6 +1,7 @@
 
 #include <array>
 #include <cstddef>
+#include <cstdint>
 #include <string_view>
 #include <utility>
 
@@ -10,6 +11,8 @@
 #include <benchmark/benchmark.h>
 
 #include "./benchmark_helpers.hpp"
+#include "twig/datadog/mpmc_client.hpp"
+#include "twig/datadog/spsc_client.hpp"
 #include "twig/datadog/tags.hpp"
 #include "twig/datadog/udp_client.hpp"
 
@@ -19,25 +22,44 @@ namespace twig::datadog
 namespace
 {
 
+template<typename T>
+auto create_client(asio::io_context& context) -> T
+{
+    constexpr static auto hostname = std::string_view {"localhost"};
+    constexpr static auto port = int16_t {18125};
+    constexpr static auto queue_size = std::size_t {1'000'000};
+
+    if constexpr (std::is_same_v<T, Client>) {
+        return Client(UDPClient(context, hostname, port));
+    } else if constexpr (std::is_same_v<T, SPSCClient>) {
+        return SPSCClient(Client(UDPClient(context, hostname, port)), queue_size);
+    } else if constexpr (std::is_same_v<T, MPMCClient>) {
+        return MPMCClient(Client(UDPClient(context, hostname, port)), queue_size);
+    }
+}
+
+template<typename T>
 auto benchmark_client_send_async(benchmark::State& state) -> void
 {
     auto values = random_double_vector(100, 0.0, 1'000'000.0);
 
     auto context = asio::io_context();
-    auto client = Client(UDPClient(context, "localhost", 18125));
+    auto client = create_client<T>(context);
 
     auto iteration = std::size_t {0};
     for (auto _ : state) {
         auto tags = Tags::create(std::array<std::string_view, 2> {"tag1:hello", "tag2:world"});
-        auto gauge = Gauge("metric_name", values[iteration % values.size()]).with_tags(std::move(tags));
 
-        client.send_async(gauge);
+        client.send_async(
+            std::move(Gauge("metric_name", values[iteration % values.size()]).with_tags(std::move(tags))));
         iteration++;
     }
 }
 
 }  // namespace
 
-BENCHMARK(benchmark_client_send_async);
+BENCHMARK(benchmark_client_send_async<Client>);
+BENCHMARK(benchmark_client_send_async<SPSCClient>)->Iterations(1'000'000);
+BENCHMARK(benchmark_client_send_async<MPMCClient>)->Iterations(1'000'000);
 
 }  // namespace twig::datadog
