40fbd3f8d46ef7ade2ea09e5b79da671c9938c2b
Mikael Lindemann <mikael@twig.energy>
2024-09-16 14:01:41 +0200

    ...

--- cmake/install-config.cmake
+++ cmake/install-config.cmake
@@ -3,6 +3,6 @@ find_dependency(absl)
 find_dependency(asio)
 find_dependency(fmt)
 find_dependency(MPMCQueue)
-find_dependency(SPSCqueue)
+find_dependency(SPSCQueue)
 
 include("${CMAKE_CURRENT_LIST_DIR}/cpp-datadog-clientTargets.cmake")
