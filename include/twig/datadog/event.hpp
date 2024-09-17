68a2b7e51ca81d87a1b731642bb1136ff62f1bc4
Mikael Lindemann <mikael@twig.energy>
2024-09-17 13:44:49 +0200

    this prefix

--- include/twig/datadog/event.hpp
+++ include/twig/datadog/event.hpp
@@ -43,19 +43,19 @@ struct Event
 
     constexpr auto with(Tags tags_) -> Event&
     {
-        tags = std::move(tags_);
+        this->tags = std::move(tags_);
         return *this;
     }
 
     constexpr auto with(Priority priority_) -> Event&
     {
-        priority = priority_;
+        this->priority = priority_;
         return *this;
     }
 
     constexpr auto with(AlertType alert_type_) -> Event&
     {
-        alert_type = alert_type_;
+        this->alert_type = alert_type_;
         return *this;
     }
 
