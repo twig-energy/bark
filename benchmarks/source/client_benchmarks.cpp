ff64d10a250e1de604600832a60fd94895d4bedf
Mikael Lindemann <mikael@twig.energy>
2024-09-17 08:28:52 +0200

    ...

--- benchmarks/source/client_benchmarks.cpp
+++ benchmarks/source/client_benchmarks.cpp
@@ -47,7 +47,7 @@ auto benchmark_client_send_async(benchmark::State& state) -> void
 
     auto iteration = std::size_t {0};
     for (auto _ : state) {
-        auto tags = Tags::from_tags({{"tag1:hello", "tag2:world"}});
+        auto tags = Tags::from_tags({"tag1:hello", "tag2:world"});
 
         client.send_async(
             std::move(Gauge("metric_name", values[iteration % values.size()]).with_tags(std::move(tags))));
