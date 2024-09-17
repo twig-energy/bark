47377935dde15c6836778d89cb4956a1d10be70e
Anders Wind <anders.wind@twig.energy>
2024-09-17 13:24:14 +0200

    Added a single include header to make consumption easier

---
+++ include/twig/datadog/datadog-client.hpp
@@ -0,0 +1,17 @@
+#pragma once
+/**
+ * Single header for including all the necessary headers for the Datadog client.
+ * Consider including the individual headers instead.
+ *
+ */
+
+#include "twig/datadog/client.hpp"
+#include "twig/datadog/count.hpp"
+#include "twig/datadog/gauge.hpp"
+#include "twig/datadog/histogram.hpp"
+#include "twig/datadog/i_datadog_client.hpp"
+#include "twig/datadog/metric.hpp"
+#include "twig/datadog/noop_client.hpp"
+#include "twig/datadog/spsc_client.hpp"
+#include "twig/datadog/tags.hpp"
+#include "twig/datadog/udp_client.hpp"
