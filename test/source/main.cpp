a4d1dc34bbc5f7759b4919eb295e99d09de3b9a6
Anders Wind <anders.wind@twig.energy>
2026-01-06 11:43:51 +0100

    Allow input which is convertible to double in Gauge and Histogram (#20)
    ### Why 
    To make the usage of the library more ergonomic.
    
    ### What was changed
    - Changed constructors of Gauge and Histogram to receive any value and
    then static_cast it to double.

--- test/source/main.cpp
+++ test/source/main.cpp
@@ -1,15 +1,2 @@
-#include <string>
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
-
-namespace bark
-{
-
-// doctest::detail::g_cs->currentTest->m_name is only accessible in the compilation unit that has
-// DOCTEST_CONFIG_IMPLEMENT or DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN defined.
-auto current_test_name() -> std::string
-{
-    return doctest::detail::g_cs->currentTest->m_name;
-}
-
-}  // namespace bark
