3a2121ba49dcf8e61d4a035b5d2a4fe61389ee20
Mikael Lindemann <359941+mikaellindemann@users.noreply.github.com>
2026-01-25 15:41:08 +0100

    Add operator== to data structs. (#22)
    ### Why 
    It is quite cumbersome to make mock expectations on calls to the datadog
    client, because the metrics are not equatable.
    
    This seeks to fix that.
    
    ### What was changed
    * Added operator== to a bunch of data structs.

--- include/bark/number_of_io_threads.hpp
+++ include/bark/number_of_io_threads.hpp
@@ -2,12 +2,16 @@
 
 #include <cstddef>
 
+#include "feature_detection.hpp"
+
 namespace bark
 {
 
 struct NumberOfIOThreads
 {
     std::size_t value;
+
+    BARK_CONSTEXPR auto operator==(const NumberOfIOThreads&) const -> bool = default;
 };
 
 }  // namespace bark
